41979abf50ca837a39be2cdadfb6854795fd904a
Jeff Hoefs <jeff.hoefs@gmail.com>
2015-05-24 21:23:12 -0700

    add basic feature selection validation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,15 +2,44 @@ $(function () {
 
   var $tooltip = $('[data-toggle="tooltip"]');
   var $form = $('form');
+  var $submitBtn = $('#submit');
+  var $features = $('.features');
+  var $featureCheckboxes = $features.find("input:checkbox");
+  var $featureAlert = $('.feature-alert');
+
+  function getNumFeaturesChecked() {
+    return $features.find("input:checkbox:checked").length;
+  }
+
+  function validateFeatureSelection($formData) {
+    numSelected = getNumFeaturesChecked();
+    if (numSelected > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   $tooltip.on('click', function (e) {
     e.preventDefault();
   });
   $tooltip.tooltip();
 
+  $featureCheckboxes.on('click', function (e) {
+    // if the user selects a feature when the feature alert is visible, hide the alert
+    if (getNumFeaturesChecked() > 0) {
+      if (!$featureAlert.is(':hidden')) {
+        $featureAlert.hide();
+      }
+    }
+  });
+
   $form.on('submit', function (e) {
-    //console.log("submit form");
-    //e.preventDefault();
+
+    if (!validateFeatureSelection($(this))) {
+      e.preventDefault();
+      $featureAlert.show();
+    }
   });
 
 });
